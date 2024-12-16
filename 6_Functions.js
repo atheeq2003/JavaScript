cdb344554aa406679e012f20a2d682405b0d4462
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-12-16 20:02:18 +0530

    Functions beginning

---
+++ 6_Functions.js
@@ -0,0 +1,10 @@
+// Defining Function
+function printStmnt() {
+    console.log("log through printStmnt Method: ")
+    console.log("Javascript....");
+}
+
+// 1st Function call
+printStmnt();
+// 2nd Function call
+printStmnt();
